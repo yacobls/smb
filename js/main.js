acaad800dbc712dbc084045feab2ff66d998ec76
yacobls <yacob.lamrodo@gmail.com>
2024-05-16 03:06:16 +0700

    16-05-2024

--- js/main.js
+++ js/main.js
@@ -324,3 +324,24 @@
 
 })(jQuery);
 
+document.addEventListener('contextmenu', function(event) {
+	event.preventDefault();
+  });
+
+  document.onkeydown = function(e) {
+	if(e.keyCode == 123) { // F12 key
+	  return false;
+	}
+	if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+	  return false;
+	}
+	if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+	  return false;
+	}
+	if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+	  return false;
+	}
+	if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+	  return false;
+	}
+  }
