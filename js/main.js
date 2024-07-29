970e2120743f00a1fffbf36946522c5bb75e29d1
yacobls <yacob.lamrodo@gmail.com>
2024-07-30 03:55:13 +0700

    30-07-2024

--- js/main.js
+++ js/main.js
@@ -344,6 +344,28 @@ AOS.init({
   $("#time_pick").timepicker();
 })(jQuery);
 
+document.addEventListener('contextmenu', function(event) {
+	event.preventDefault();
+  });
+
+  document.onkeydown = function(e) {
+	if(e.keyCode == 123) { // F12 key
+	  return false;
+	}
+	if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+	  return false;
+	}
+	if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+	  return false;
+	}
+	if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+	  return false;
+	}
+	if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+	  return false;
+	}
+  }
+
 document.querySelector(".wa-button").addEventListener("click", function () {
   // Contoh pelacakan klik menggunakan Google Analytics
   gtag("event", "click", {
@@ -352,25 +374,3 @@ document.querySelector(".wa-button").addEventListener("click", function () {
     value: "1",
   });
 });
-
-// document.addEventListener('contextmenu', function(event) {
-// 	event.preventDefault();
-//   });
-
-//   document.onkeydown = function(e) {
-// 	if(e.keyCode == 123) { // F12 key
-// 	  return false;
-// 	}
-// 	if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-// 	  return false;
-// 	}
-// 	if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
-// 	  return false;
-// 	}
-// 	if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
-// 	  return false;
-// 	}
-// 	if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
-// 	  return false;
-// 	}
-//   }
